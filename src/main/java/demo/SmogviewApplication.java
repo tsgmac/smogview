abe2052d428eda2b7a4c0681f5afd452a1033f62
user01 <user01@asgard>
2015-01-24 13:15:10 +0100

    Initialization

---
+++ src/main/java/demo/SmogviewApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmogviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmogviewApplication.class, args);
+    }
+}
