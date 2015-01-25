c64176e19768a61dd27239060f0e44aa0d3b8148
user01 <user01@asgard>
2015-01-25 16:56:58 +0100

    Prepared filtering

--- src/main/resources/static/app/smog.js
+++ src/main/resources/static/app/smog.js
@@ -7,9 +7,9 @@
 //}
 
 //and now the AngularJS 1.3 way
-angular.module('smogApp', []).controller('Smog', function ($scope, $http) {
-    $http.get('http://localhost:8080/measurements/all').
-        success(function(data) {
-        	$scope.measurements = angular.fromJson(data);           
-        });
+angular.module('smogApp', []).controller('Smog', function($scope, $http) {
+	$http.get('http://localhost:8080/measurements/all').success(function(data) {
+		$scope.measurements = angular.fromJson(data);
+		$scope.predicate = 'utcDate';
+	});
 });
