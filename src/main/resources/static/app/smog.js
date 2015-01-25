9b484f7c2e0762cd89cbf6fc1a5494acd7428a6d
user01 <user01@asgard>
2015-01-25 18:37:33 +0100

    clean up

--- src/main/resources/static/app/smog.js
+++ src/main/resources/static/app/smog.js
@@ -1,12 +1,3 @@
-//works until AngularJS 1.2.27 (inclusive)
-//function Smog($scope, $http) {
-//	$http.get('http://localhost:8080/measurements/all').success(function(data) {
-//			var response = angular.fromJson(data);
-//			$scope.measurements = response;
-//	});
-//}
-
-//and now the AngularJS 1.3 way
 angular.module('smogApp', []).controller('Smog', function($scope, $http) {
 	$http.get('http://localhost:8080/measurements/all').success(function(data) {
 		$scope.measurements = angular.fromJson(data);
