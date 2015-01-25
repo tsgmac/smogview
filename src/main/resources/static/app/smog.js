d361129e02886ac775e69dbe090b1c2fad9f30be
user01 <user01@asgard>
2015-01-25 15:27:36 +0100

    Now with AngularJS 1.3.10 and Twitter Bootstrap 3.3.2

--- src/main/resources/static/app/smog.js
+++ src/main/resources/static/app/smog.js
@@ -1,8 +1,15 @@
-//angular.module('Smog', []).controller('Smog', [function($scope, $http) { 
-function Smog($scope, $http) {
-	$http.get('http://localhost:8080/measurements/all').success(function(data) {
-			var response = angular.fromJson(data);
-			$scope.measurements = response;
-	});
-}
-//}])
+//works until AngularJS 1.2.27 (inclusive)
+//function Smog($scope, $http) {
+//	$http.get('http://localhost:8080/measurements/all').success(function(data) {
+//			var response = angular.fromJson(data);
+//			$scope.measurements = response;
+//	});
+//}
+
+//and now the AngularJS 1.3 way
+angular.module('smogApp', []).controller('Smog', function ($scope, $http) {
+    $http.get('http://localhost:8080/measurements/all').
+        success(function(data) {
+        	$scope.measurements = angular.fromJson(data);           
+        });
+});
