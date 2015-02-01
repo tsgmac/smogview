0b086d9957ace4b6dfe6c5ca4a48857975eb398e
user01 <user01@asgard>
2015-02-01 16:07:33 +0100

    Fixed sorting of dates and times and added dropdown list and functionality.

--- src/main/resources/static/app/smog.js
+++ src/main/resources/static/app/smog.js
@@ -1,6 +1,6 @@
-angular.module('smogApp', []).controller('Smog', function($scope, $http) {
+angular.module('smogApp', ['ui.utils']).controller('Smog', function($scope, $http) {
 	$http.get('http://localhost:8080/measurements/all').success(function(data) {
 		$scope.measurements = angular.fromJson(data);
-		$scope.predicate = 'utcDate';
+		$scope.predicate = 'utcDate';		
 	});
 });
